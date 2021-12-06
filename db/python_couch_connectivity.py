9a46aa0816ac4c26f252cc08eaef3a884d48d1ba
unknown <nina.heuzer@student.vinci.be>
2021-12-06 15:52:35 +0100

    add connectivity to couchDb + create database + create 4 documents

---
+++ db/python_couch_connectivity.py
@@ -0,0 +1,22 @@
+import couchdb
+
+USERNAME = "admin"
+PASSWORD = "SNKBN"
+# connecting with couchdb server
+couch = couchdb.Server('http://%s:%s@localhost:5984'%(USERNAME, PASSWORD))
+
+# creating database
+db = couch.create("pfe-df-g4")
+print("database \'pfe-df-g4\' created")
+
+#creating document
+docAnnounces = {'name':'announces'}
+docUsers = {'name':'users'}
+docCategories = {'name':'categories'}
+docAddresses = {'name':'addresses'}
+listDocs = [docAnnounces, docUsers, docCategories, docAddresses]
+for doc in listDocs:
+    db.save(doc)
+    print("document \'" + doc['name'] + "\' created")
+    #fetching from the database
+    print("name is : " + doc['name'])
