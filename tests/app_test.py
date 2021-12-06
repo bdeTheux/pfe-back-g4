b9b91dc3e6316aef88589fd9e4401356fdf0fc2b
Kevin <kevin.jullien@student.vinci.be>
2021-12-06 12:15:49 +0100

    refactor - usage of pytest instead of nose

--- tests/app_test.py
+++ tests/app_test.py
@@ -3,10 +3,8 @@ To run the tests type,
 $ nosetests --verbose
 """
 
-from nose.tools import assert_true
-
 BASE_URL = "http://localhost:5000"
 
 
 def test_always_true():
-    assert_true(True)
+    assert True == True
