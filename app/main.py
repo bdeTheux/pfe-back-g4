24ba9e27045a802e783d3c189e637d65ed8cba35
Kevin <kevin.jullien@student.vinci.be>
2021-12-06 11:19:27 +0100

    add - gunicorn for pipeline

--- app/main.py
+++ app/main.py
@@ -4,36 +4,36 @@ from flask import Flask, jsonify, make_response
 from routes import request_api
 import dotenv
 
-APP = Flask(__name__)
+app = Flask(__name__)
 
-APP.register_blueprint(request_api.get_blueprint())
+app.register_blueprint(request_api.get_blueprint())
 
 
-@APP.errorhandler(400)
+@app.errorhandler(400)
 def handle_400_error(_error):
     """Return a http 400 error to client"""
     return make_response(jsonify({'error': 'Misunderstood'}), 400)
 
 
-@APP.errorhandler(401)
+@app.errorhandler(401)
 def handle_401_error(_error):
     """Return a http 401 error to client"""
     return make_response(jsonify({'error': 'Unauthorised'}), 401)
 
 
-@APP.errorhandler(404)
+@app.errorhandler(404)
 def handle_404_error(_error):
     """Return a http 404 error to client"""
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@APP.errorhandler(500)
+@app.errorhandler(500)
 def handle_500_error(_error):
     """Return a http 500 error to client"""
     return make_response(jsonify({'error': 'Server error'}), 500)
 
 
-envFile = dotenv.dotenv_values(".env")
+envFile = dotenv.dotenv_values("../.env")
 port = envFile.get("port")
 print(envFile.get("port"))
 print(__name__)
@@ -49,8 +49,8 @@ if __name__ == '__main__':
 
     if ARGS.debug:
         print("Running in debug mode")
-        CORS = CORS(APP)
-        APP.run(host='0.0.0.0', port=port, debug=True)
+        CORS = CORS(app)
+        app.run(host='0.0.0.0', port=port, debug=True)
     else:
         print("test")
-        APP.run(host='0.0.0.0', port=port, debug=False)
+        app.run(host='0.0.0.0', port=port, debug=False)
