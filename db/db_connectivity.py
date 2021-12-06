7a82fdd46b1cbb8158d59843101b2958ab1fbc3e
Kevin <kevin.jullien@student.vinci.be>
2021-12-06 16:23:16 +0100

    refactor - generic file name

--- db/db_connectivity.py
+++ db/db_connectivity.py
@@ -3,20 +3,20 @@ import couchdb
 USERNAME = "admin"
 PASSWORD = "SNKBN"
 # connecting with couchdb server
-couch = couchdb.Server('http://%s:%s@localhost:5984'%(USERNAME, PASSWORD))
+couch = couchdb.Server('http://%s:%s@localhost:5984' % (USERNAME, PASSWORD))
 
 # creating database
 db = couch.create("pfe-df-g4")
 print("database \'pfe-df-g4\' created")
 
-#creating document
-docAnnounces = {'name':'announces'}
-docUsers = {'name':'users'}
-docCategories = {'name':'categories'}
-docAddresses = {'name':'addresses'}
+# creating document
+docAnnounces = {'name': 'announces'}
+docUsers = {'name': 'users'}
+docCategories = {'name': 'categories'}
+docAddresses = {'name': 'addresses'}
 listDocs = [docAnnounces, docUsers, docCategories, docAddresses]
 for doc in listDocs:
     db.save(doc)
     print("document \'" + doc['name'] + "\' created")
-    #fetching from the database
+    # fetching from the database
     print("name is : " + doc['name'])
