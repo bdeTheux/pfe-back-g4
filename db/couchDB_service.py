fb68d022bed65e4e6bfce8283359ced0af81855e
Kevin <kevin.jullien@student.vinci.be>
2021-12-07 17:28:38 +0100

    fix attempt 2 - creating db if it does not exists already

--- db/couchDB_service.py
+++ db/couchDB_service.py
@@ -18,7 +18,12 @@ else:
     host = envfile.get("DBProdHost")
 
 server = couchdb.Server('http://%s:%s@%s:5984' % (username, password, host))
-database = server["pfe-df-g4"]
+try:
+    database = server["pfe-df-g4"]
+except:
+    server.create("pfe-df-g4")
+    database = server["pfe-df-g4"]
+
 documents = database.get("_all_docs")
 
 
