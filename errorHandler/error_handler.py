07e4d01f2296f150bedfdb8f76b3b51df386022a
Kevin <kevin.jullien@student.vinci.be>
2021-12-09 09:07:25 +0100

    refactor - error handler

--- errorHandler/error_handler.py
+++ errorHandler/error_handler.py
@@ -1,26 +1,5 @@
 from flask import jsonify, make_response
 
 
-def handle_400_error(_error):
-    """Return a http 400 error to client"""
-    return make_response(jsonify({'error': _error.description}), 400)
-
-
-def handle_401_error(_error):
-    """Return a http 401 error to client"""
-    return make_response(jsonify({'error': _error.description}), 401)
-
-
-def handle_404_error(_error):
-    """Return a http 404 error to client"""
-    return make_response(jsonify({'error': _error.description}), 404)
-
-
-def handle_405_error(_error):
-    """Return a http 405 error to client"""
-    return make_response(jsonify({'error': _error.description}), 405)
-
-
-def handle_500_error(_error):
-    """Return a http 500 error to client"""
-    return make_response(jsonify({'error': _error.description}), 500)
+def generic_http_handler(_error):
+    return make_response(jsonify({'error': _error.description}), _error.code)
