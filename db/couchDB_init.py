a1d2fea15ae367730593627004a54ec47a675b82
Kevin <kevin.jullien@student.vinci.be>
2021-12-12 13:28:54 +0100

    refactor - 'En vente' -> 'À vendre'

--- db/couchDB_init.py
+++ db/couchDB_init.py
@@ -115,7 +115,7 @@ elif response == 'init':
 
     # POSTS
     giving = "À donner"
-    selling = "En vente"
+    selling = "À vendre"
 
     pending = 'En attente d\'approbation'
     approved = 'Approuvé'
@@ -170,8 +170,7 @@ elif response == 'init':
                                 description="Non, peut-être?",
                                 places=['Louvain-la-Neuve'],
                                 seller_id=samy, price=0,
-                                category_id='Outils')
-
+                                category_id='Vêtements et chaussures femmes')
 
     # Closed
     db[uuid.uuid4().hex] = dict(type='Post', post_nature=giving, state=closed,
@@ -187,15 +186,6 @@ elif response == 'init':
                                 places=['Ixelles'],
                                 seller_id=nina, price=14999.9,
                                 category_id='Electroménager')
-
-
-    db['Woluwe'] = dict(type="Address", campus='Woluwe',
-                        lat="50.849857061691836", long="4.453360810918974")
-    db['Louvain-la-Neuve'] = dict(type="Address", campus='Louvain-la-Neuve',
-                                  lat="50.84981962233335", long="4.453745477375748")
-    db['Ixelles'] = dict(type="Address", campus='Ixelles',
-                         lat="50.835525846476465", long="4.376626359487836")
-
 
     # Displaying added documents
     print("Documents now in the database")
