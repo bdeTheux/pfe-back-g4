f4e4d2c65c163a177496ed25bd9127b5d9057497
Kevin <kevin.jullien@student.vinci.be>
2021-12-12 10:31:26 +0100

    add - some entities in the DB init script

--- db/couchDB_init.py
+++ db/couchDB_init.py
@@ -52,19 +52,35 @@ elif response == 'init':
     # Creating documents
     # USERS
     password = generate_password_hash("azerty")
-    id1 = uuid.uuid4().hex
-    id2 = uuid.uuid4().hex
-    id3 = uuid.uuid4().hex
-    db[id1] = dict(type='User', last_name='admin',
-                   first_name='admin', email='admin@vinci.be',
-                   password=password, campus='Ixelles', is_banned=False, is_admin=True)
-    db[id2] = dict(type='User', last_name='Jullien',
-                   first_name='Kevin', email='kevin.jullien@student.vinci.be',
-                   password=password, campus='Woluwe', is_banned=False, is_admin=False)
-    db[id3] = dict(type='User', last_name='Laraki',
-                   first_name='Narjis', email='narjis.laraki@student.vinci.be',
-                   password=password, campus='Louvain-la-Neuve', is_banned=False,
-                   is_admin=False)
+    admin = uuid.uuid4().hex
+    kevin = uuid.uuid4().hex
+    narjis = uuid.uuid4().hex
+    nina = uuid.uuid4().hex
+    boris = uuid.uuid4().hex
+    samy = uuid.uuid4().hex
+
+    db[admin] = dict(type='User', last_name='admin',
+                     first_name='admin', email='admin@vinci.be',
+                     password=password, campus='Ixelles', is_banned=False, is_admin=True)
+    db[kevin] = dict(type='User', last_name='Jullien',
+                     first_name='Kevin', email='kevin.jullien@student.vinci.be',
+                     password=password, campus='Woluwe', is_banned=False, is_admin=False)
+    db[narjis] = dict(type='User', last_name='Laraki',
+                      first_name='Narjis', email='narjis.laraki@student.vinci.be',
+                      password=password, campus='Louvain-la-Neuve', is_banned=False,
+                      is_admin=False)
+    db[nina] = dict(type='User', last_name='Heuzer',
+                    first_name='Nina', email='nina.heuzer@student.vinci.be',
+                    password=password, campus='Ixelles', is_banned=False,
+                    is_admin=False)
+    db[boris] = dict(type='User', last_name='de Theux',
+                     first_name='Boris', email='boris.detheux@student.vinci.be',
+                     password=password, campus='Louvain-la-Neuve', is_banned=False,
+                     is_admin=False)
+    db[samy] = dict(type='User', last_name='Alliche',
+                    first_name='Samy', email='samy.alliche@student.vinci.be',
+                    password=password, campus='Louvain-la-Neuve', is_banned=False,
+                    is_admin=False)
 
     # CATEGORIES
     db['Maison et Jardin'] = dict(type='Category', name='Maison et Jardin', parent=None,
@@ -98,23 +114,81 @@ elif response == 'init':
                                        parent='Vêtements et accessoires', sub_categories=[])
 
     # POSTS
-    db[uuid.uuid4().hex] = dict(type='Post', post_nature='En vente', state='En attente d\'approbation',
+    giving = "À donner"
+    selling = "En vente"
+
+    pending = 'En attente d\'approbation'
+    approved = 'Approuvé'
+    closed = 'Clôturé'
+
+    # Pending
+    db[uuid.uuid4().hex] = dict(type='Post', post_nature=giving, state=pending,
+                                title='Lacet bleu',
+                                description='Un seul lacet, 28cm',
+                                places=['Woluwe', 'Ixelles'],
+                                seller_id=kevin, price=0,
+                                category_id='Vêtements et accessoires')
+
+    db[uuid.uuid4().hex] = dict(type='Post', post_nature=selling, state=pending,
+                                title='Furet mâle',
+                                description='En forme, 3 ans, aime manger les lacets bleus',
+                                places=['Woluwe', 'Ixelles'],
+                                seller_id=kevin, price=25,
+                                category_id='Famille')
+
+    db[uuid.uuid4().hex] = dict(type='Post', post_nature=selling, state=pending,
                                 title='Converses blanches',
-                                description='En très bon état, pointure 38', address_id=['Woluwe'],
-                                seller_id=id3, price=35.5,
+                                description='En très bon état, pointure 38',
+                                places=['Woluwe'],
+                                seller_id=narjis, price=35.5,
                                 category_id='Vêtements et chaussures femmes')
-    db[uuid.uuid4().hex] = dict(type='Post', post_nature='En vente', state='Approuvé', title='Petite robe noire',
-                                description='Comme neuve', address_id=['Ixelles'],
-                                seller_id=id3, price=20,
+
+    db[uuid.uuid4().hex] = dict(type='Post', post_nature=selling, state=pending,
+                                title='Éperons portés par Billy the kid',
+                                description="Tout est dans le titre, c'est pas un fake!",
+                                places=['Woluwe', 'Louvain-la-Neuve'],
+                                seller_id=nina, price=120000.3,
+                                category_id='Fournitures pour animaux')
+
+    # Accepted
+    db[uuid.uuid4().hex] = dict(type='Post', post_nature=selling, state=approved,
+                                title='Petite robe noire',
+                                description='Comme neuve',
+                                places=['Ixelles'],
+                                seller_id=narjis, price=20,
                                 category_id='Vêtements et chaussures femmes')
-    db[uuid.uuid4().hex] = dict(type='Post', post_nature='À donner', state='Clôturé', title='Vieux paquet de chips',
-                                description='Il en reste 3, paprika', address_id=['Ixelles'],
-                                seller_id=id3, price=0,
+
+    db[uuid.uuid4().hex] = dict(type='Post', post_nature=selling, state=approved,
+                                title='Recette de cocktail',
+                                description='Une base gingembre, évidemment! De quoi rester en bonne santé',
+                                places=['Woluwe'],
+                                seller_id=boris, price=1.5,
+                                category_id='Vêtements et chaussures femmes')
+
+    db[uuid.uuid4().hex] = dict(type='Post', post_nature=giving, state=approved,
+                                title='Talent',
+                                description="Non, peut-être?",
+                                places=['Louvain-la-Neuve'],
+                                seller_id=samy, price=0,
+                                category_id='Vêtements et chaussures femmes')
+
+    # Closed
+    db[uuid.uuid4().hex] = dict(type='Post', post_nature=giving, state=closed,
+                                title='Vieux paquet de chips',
+                                description='Il en reste 3, paprika, à peine humides',
+                                places=['Ixelles'],
+                                seller_id=kevin, price=0,
                                 category_id='Santé et beauté')
 
-    db['Woluwe'] = dict(type="Address", campus='Woluwe',
-                        lat="50.849857061691836", long="4.453360810918974")
-    db['Louvain-la-Neuve'] = dict(type="Address", campus='Louvain-la-Neuve',
-                                  lat="50.84981962233335", long="4.453745477375748")
-    db['Ixelles'] = dict(type="Address", campus='Ixelles',
-                         lat="50.835525846476465", long="4.376626359487836")
+    db[uuid.uuid4().hex] = dict(type='Post', post_nature=selling, state=closed,
+                                title='Mazda rouge',
+                                description='Superbe voiture',
+                                places=['Ixelles'],
+                                seller_id=nina, price=14999.9,
+                                category_id='Electroménager')
+
+    # Displaying added documents
+    print("Documents now in the database")
+    db = couch["pfe-df-g4"]
+    for doc in db:
+        print(db[doc])
