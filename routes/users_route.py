3e8220e4ee34c01563e0b8bcaf21540447d14567
Narjis <narjis.laraki@student.vinci.be>
2021-12-14 14:54:25 +0100

    refactor - typo

--- routes/users_route.py
+++ routes/users_route.py
@@ -58,7 +58,7 @@ def edit_one(current_user):
 
     data = request.json
     email = data.get('email', current_user['email'])
-    first_name = data.get('first_name', current_user['email'])
+    first_name = data.get('first_name', current_user['first_name'])
     last_name = data.get('last_name', current_user['last_name'])
     campus = data.get('campus', current_user['campus'])
     password = data.get('password', current_user['password'])
